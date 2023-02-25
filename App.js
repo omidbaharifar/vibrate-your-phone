0bdd6a18651626d01c9ff867e95d8b0eb7a10b9f
omidbaharifar <weblax.ir@gmail.com>
2023-02-25 20:37:20 +0330

    add useEffect for state

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, View, Vibration } from "react-native";
 
 const App = () => {
   const [isVibratorOn, setIsVibratorOn] = useState(false);
+  useEffect(() => {
+    isVibratorOn ? Vibration.vibrate([1000], true) : Vibration.cancel();
+  }, [isVibratorOn]);
 
   return (
-    <View>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Button
         onPress={() => {
           setIsVibratorOn(!isVibratorOn);
-          isVibratorOn ? Vibration.vibrate([1000], true) : Vibration.cancel();
         }}
         title={isVibratorOn ? "خاموش" : "روشن"}
       />
